d920b20553d89a412ac6050d1ddcf0044f4de848
raining <running.zhang@foxmail.com>
2024-09-08 23:02:04 +0800

    fix：修改个人中心侧边栏链接标签过小的问题

--- src/views/user_center/components/AsideMenu.vue
+++ src/views/user_center/components/AsideMenu.vue
@@ -2,16 +2,16 @@
     <div class="menuContainer">
         <el-menu default-active="1" class="menu" @open="handleOpen" @close="handleClose">
             <el-menu-item index="1">
-                <router-link to="/user_info">个人信息</router-link>
+                <router-link to="/user_info" class="linkBox">个人信息</router-link>
             </el-menu-item>
             <el-menu-item index="2">
-                <router-link to="/change_pass">修改密码</router-link>
+                <router-link to="/change_pass" class="linkBox">修改密码</router-link>
             </el-menu-item>
             <el-menu-item index="3">
-                <router-link to="/my_address">我的地址</router-link>
+                <router-link to="/my_address" class="linkBox">我的地址</router-link>
             </el-menu-item>
             <el-menu-item index="4">
-                <router-link to="/orders">我的订单</router-link>
+                <router-link to="/orders" class="linkBox">我的订单</router-link>
             </el-menu-item>
         </el-menu>
     </div>
@@ -32,7 +32,13 @@ const handleClose = () => { }
     .menu {
         height: 100%;
     }
-
+    li.el-menu-item{
+        padding: 0 !important;
+    }
+    .linkBox{
+        width: 100%;
+        padding: 0 20px;
+    }
 
 }
 </style>
