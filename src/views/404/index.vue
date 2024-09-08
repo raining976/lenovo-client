250a8788fe215b867d4783af297e4d4a9171b6ba
raining <running.zhang@foxmail.com>
2024-09-08 23:02:43 +0800

    refactor：美化404，未授权跳转登录

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,8 +1,30 @@
 <template>
-    404
+    <div class="container">
+        <h3>404，您访问的页面不存在!</h3>
+    </div>
 </template>
 
 <script setup>
 </script>
-<style scoped>
+<style lang="scss" scoped>
+.container {
+    width: 100%;
+
+    height: calc(100vh -  $navHeight);
+    display: flex;
+    align-items: center;
+    /*定义body的元素垂直居中*/
+    justify-content: center;
+    /*定义body的里的元素水平居中*/
+    background-color: #eee;
+    font-size: 14px;
+}
+
+h3 {
+    font-size: 60px;
+    color: #cf3c32;
+    text-align: center;
+    padding-top: 30px;
+    font-weight: normal;
+}
 </style>
