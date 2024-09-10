5248cbcf6ebe14a17f83abf362e669cb07f20262
raining <running.zhang@foxmail.com>
2024-09-11 00:18:45 +0800

    feat：添加删除地址功能

--- src/api/modules/user.js
+++ src/api/modules/user.js
@@ -4,5 +4,5 @@ import { get, post} from "@/utils/request";
 export const getUserInfo = () => get('/user/info')
 export const getAddress = (data) => get('/address/list',data)
 export const createAddress = (data) => post('/address/create',data, true)
-
+export const delAddress = (data) => get('/address/delete',data, true)
 export const uploadAvatar = (data) => post('/user/avatar',data, true)
--- src/views/user_center/views/my_address.vue
+++ src/views/user_center/views/my_address.vue
@@ -5,13 +5,24 @@
         </div>
         <el-divider />
         <div class="addressList">
-            <el-descriptions class="addressItem"  :column="column" :title="`地址 ${index + 1}`" v-for="(address, index) in addressList" :key="index" v-show="address.name">
-                <el-descriptions-item label="姓名">{{ address.name }}</el-descriptions-item>
-                <el-descriptions-item label="电话">{{ address.phone }}</el-descriptions-item>
-                <el-descriptions-item label="地址">
-                    {{ address.dz }}
-                </el-descriptions-item>
-            </el-descriptions>
+            <li class="addressItemLi" v-for="(address, index) in addressList" :key="index" v-show="address.name">
+                <el-card style="width: 100%" shadow="always">
+                    <el-descriptions class="addressItem" :column="column" :title="`地址 ${index + 1}`">
+                        <el-descriptions-item label="姓名">{{ address.name }}</el-descriptions-item>
+                        <el-descriptions-item label="电话">{{ address.phone }}</el-descriptions-item>
+                        <el-descriptions-item label="地址">
+                            {{ address.dz }}
+                        </el-descriptions-item>
+                    </el-descriptions>
+                </el-card>
+                <el-popconfirm title="确定要删？？？" @confirm="confirmDel(address.id)">
+                    <template #reference>
+                        <el-button type="danger" :icon="Delete" circle />
+                    </template>
+                </el-popconfirm>
+               
+            </li>
+
         </div>
     </div>
 
@@ -19,6 +30,7 @@
 <script setup>
 import AddressForm from "../components/AddressForm.vue";
 import { useUserStore } from "@/store"
+import { Delete } from '@element-plus/icons-vue'
 const { proxy } = getCurrentInstance()
 const userStore = useUserStore()
 
@@ -41,17 +53,37 @@ const updateAddress = () => {
 }
 
 
-
+const confirmDel = (id)=>{
+    console.log('id',id)
+    proxy.$api.delAddress({Id:id}).then(res=>{
+        if(res.code == 0){
+            updateAddress()
+        }
+    })
+}
 
 </script>
 
 <style lang="scss" scoped>
-.addressList{
+.addressList {
     display: flex;
     flex-wrap: wrap;
-    .addressItem{
-        min-width: 350px;
+
+    .addressItemLi {
+        position: relative;
+        margin-right: 30px;
+        margin-bottom: 30px;
+
+        .el-button {
+            position: absolute;
+            top: 10px;
+            right: 20px;
+            transform: scale(0.8);
+        }
+    }
+
+    .addressItem {
+        width: 380px;
     }
 }
-
 </style>
