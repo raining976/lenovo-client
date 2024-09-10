8fb2500a60955d926a5f4703a32c7927c93cc233
adele <1685679099.qq.com>
2024-09-10 16:18:38 +0800

    feat:添加了后台用户的增删改查功能

--- src/api/modules/admin.js
+++ src/api/modules/admin.js
@@ -1,10 +1,9 @@
 import { get , post} from '@/utils/request'
 
-export const adminDelOrders = (data) => post('/admin/orders/delete', data)
-export const adminDelSingleOrder = (data) => post('/admin/orders/delete', data, true)
 export const adminLogin = (data) => post('/user/admin/login',data,true)
 
 
 export const adminGetUserList = (data) => get('/admin/user/search',data)
 export const adminAddNewUser = (data) => post('/admin/user/create',data)
 export const adminDeleteUser = (data) => get('/admin/user/delete',data)
+export const adminUpdateUser = (data) => post('/admin/user/update',data)
